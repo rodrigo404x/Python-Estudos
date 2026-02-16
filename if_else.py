9c7ab3fca4d1f4aa71e6278c10a8fdb956a7ffcb
Rodrigo404 <155778305+rodrigo404x@users.noreply.github.com>
2026-02-16 19:50:35 -0300

    Exercicio de If e Else
    Algo básico, apenas para começar a entender como funciona If/Else

---
+++ if_else.py
@@ -0,0 +1,10 @@
+# Exercicio basico de if e else, onde o usuario digita dois valores e o programa compara os valores e mostra qual é maior
+
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite outro valor: ')
+
+if primeiro_valor > segundo_valor:
+    print('O primeiro valor é maior que os segundo valor')
+    
+else:
+    print('O segundo valor é maior que o primeiro valor')
